76bf0673f7f2837b75dc6b89571239b02b65a0a4
hawl29 <cz_hawl29@163.com>
2018-05-18 18:08:43 +0800

    add_annotation_fifo

--- fifo_test.c
+++ fifo_test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-typedef struct reco
+typedef struct reco             //定义结构体，用来存储页面号，和缺页标记
 {
   int no;
   int misspage;
@@ -9,14 +9,15 @@ int main()
   int n,count,no;
   printf("请输入进程分区大小: \n");
   scanf("%d",&n);
-  int que[n];
+  int que[n];                   //定义循环队列
+  int size=0                    //用size记录队列中存放元素个数，用来判断循环队列，队满队空。
   int front=0,rear=0; 
   for(int i=0;i<n;i++) que[i]=0;
   printf("请输入页号序列长度：\n");
   scanf("%d",&count);
-  reco page[count];
-  int block[n][count];               //用二维数组记录物理块中存放的页号
-  for(int i=0;i<n;i++) 
+  reco page[count];                   //reco型的数组，记录每次输入页号时，当前物理块否缺页
+  int block[n][count];                //用二维数组记录各物理块中存放过的页号
+  for(int i=0;i<n;i++)               //初始化,i表示物理块编号，j表示页面输入时的顺序
   {   
      for(int j=0;j<count;j++)
      {
@@ -24,13 +25,12 @@ int main()
      }
   }       
   printf("请依次输入页号序列:\n");
-  int size=0;
   for(int i=0;i<count;i++)
   {
      scanf("%d",&no);
      page[i].no=no;
-     for(int j=0;j<n;j++)
-     {
+     for(int j=0;j<n;j++)                  //在物理块队列中查找当前输入页号，若没找到则标记缺页，反之，未缺页
+     {                                     
        if(que[j]==no)  
        {
          page[i].misspage=0;
@@ -38,8 +38,8 @@ int main()
        }
        else page[i].misspage=1;
      }
-     if(page[i].misspage==1)               //缺页，若物理块队列不满，则将该页号插入队尾
-     {
+     if(page[i].misspage==1)               //缺页，进行页面置换。若物理块队列不满，则将该页号插入队尾
+     {                                     //未缺页，则只需记录未缺页标记即可
        if(size<n)
        {
          que[rear]=no;
